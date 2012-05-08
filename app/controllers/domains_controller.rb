b5a38c9f518d91467739ceaf3371c71bb1ae6fbe
Uko <uko@unikernel.net>
2012-05-08 20:30:04 +0300

    added some detail to domain info

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,8 +1,16 @@
+include ActionView::Helpers::DateHelper
+
 class DomainsController < ApplicationController
   def show
 		w = Whois::Client.new
 		r = w.query(params[:domain])
 		@domain = r.domain
+		@free = r.available?
+		@expiration = r.expires_on.strftime("%-d %B %Y")
+		@time_left = time_ago_in_words(r.expires_on)
+		@status = r.status
+		@nss = r.nameservers
+		@contact = r.registrant_contact
   end
 
   def new
--- app/helpers/domains_helper.rb
+++ app/helpers/domains_helper.rb
@@ -1,2 +1,10 @@
 module DomainsHelper
+	
+	def add_list_section(section, data)
+		if data
+			haml_tag :dt, section
+			haml_tag :dd, data
+		end
+	end
+	
 end
