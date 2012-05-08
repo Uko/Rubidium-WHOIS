983e5e54589adeccaf961c3ecd4f9eba6f60cbbb
Uko <uko@unikernel.net>
2012-05-08 13:47:19 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whois::Application.config.secret_token = '9c12cc32881d530a072f00b1b00b49b282aef1343f2784f3eaf4783112143b5a06eb3af4f5c50cf744471f9ba4ee170f8a94e5a8b3ec32b9b37fe0fa076ca343'
