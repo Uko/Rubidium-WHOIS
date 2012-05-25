3d75b31220191e7439c3507960c3106ecfdf4012
Uko <uko@unikernel.net>
2012-05-25 12:14:05 +0300

    Added search(new) view/action, styling and a lot of stuff

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,31 +1,23 @@
 class DomainsController < ApplicationController
   def show
-		if params[:zone].nil?
-			if params[:domain_request].nil?
-				redirect_to :action => :new
-			else
-				redirect_to "/" << params[:domain_request].split('.').reverse.join('/')
-			end
-		end
 		dns = "#{params[:domain].split('/').reverse.join('.')}.#{params[:zone]}"
 		w = Whois::Client.new
 		@r = w.query(dns)
-		@domain = @r.domain || dns
-		@free = @r.available?
-		@free_class = @free ? "free" : "taken"
-		if not @free 
-			@expiration = @r.expires_on.strftime("%-d %B %Y")
-			@time_left = time_ago_in_words(@r.expires_on)
-			@status = @r.status
-			@nss = @r.nameservers
-			@contacts =
-			{
-				"Registrant Contacts" => @r.registrant_contact,
-				"Admin Contacts" => @r.admin_contact,
-				"Technical Contacts" => @r.technical_contact
-			}
-		end
+		@r.domain ||= dns
   end
+
+	def create
+		if not params.key? :domain_request or params[:domain_request].blank?
+			redirect_to :action => :new
+		else
+			unless (params[:domain_request].strip =~ /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}$/ix).nil?
+				redirect_to "/domains/" << params[:domain_request].split('.').reverse.join('/')
+			else
+				flash[:error] = "\"#{params[:domain_request].strip}\" is not a valid domain name"
+				redirect_to :action => :new
+			end
+		end
+	end
 
   def new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	
+	def menu_item	(name, of_controller, to_action)
+		haml_tag :li, :class => ("active" if controller.controller_name == of_controller && controller.action_name == to_action) do
+			haml_concat link_to(name, :controller => of_controller, :action => to_action)
+		end
+	end
+	
 end
