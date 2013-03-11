580cdcade8fa4daf9d5df2ab31fa6654cbef6c8d
Uko <uko@unikernel.net>
2013-03-11 18:31:01 +0100

    Completely rewrote script

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -1,13 +1,15 @@
 echo -e "Starting to update gh-pages\n"
 
-git fetch -n origin
-git checkout -b gh-pages origin/gh-pages
+cp -R coverage $HOME/coverage
 
-cp -rf coverage/* .
-
-git remote set-url origin https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git > /dev/null
+cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages > /dev/null
+
+cd gh-pages
+mv $HOME/coverage/* .
+
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
