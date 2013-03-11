54713ee896d6a86665d14ae93734e7a6feca8e81
Uko <uko@unikernel.net>
2013-03-11 18:53:47 +0100

    Added guard for pull requests

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -1,17 +1,19 @@
-echo -e "Starting to update gh-pages\n"
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
 
-cp -R coverage $HOME/coverage
+  cp -R coverage $HOME/coverage
 
-cd $HOME
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages > /dev/null
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages > /dev/null
 
-cd gh-pages
-mv -f $HOME/coverage/* .
+  cd gh-pages
+  mv -f $HOME/coverage/* .
 
-git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-git push -fq origin gh-pages > /dev/null
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
 
-echo -e "Done magic with coverage\n"
+  echo -e "Done magic with coverage\n"
+fi
