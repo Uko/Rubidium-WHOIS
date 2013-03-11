7055f6c51192bf3a37327892502026365d1726e4
Uko <uko@unikernel.net>
2013-03-11 17:21:30 +0100

    Added dedicated after-script

---
+++ .utility/update-gh-pages.sh
@@ -0,0 +1,15 @@
+echo -e "Starting to update gh-pages\n"
+
+git fetch -n origin
+git checkout gh-pages
+
+cp -rf coverage/* .
+
+git remote set-url origin https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git > /dev/null
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Done magic with coverage\n"
